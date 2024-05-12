545f634ed83fb27f16f6122803468e3bf7c6943d
Ksenia Porfirieva <porfirieva.k@mail.ru>
2024-05-12 22:08:30 +0700

    главная страниа слайдер

--- src/Pages/Main/index.js
+++ src/Pages/Main/index.js
@@ -1,12 +1,35 @@
+import Slider from "react-slick";
+
 import MenuList from "../../components/MenuList";
-import mainImage from "./location_SeasonsUp.png";
+import example from "./example.png";
+import example2 from "./example2.png";
+import example3 from "./example3.png";
+import example4 from "./example4.png";
+import example5 from "./example5.png";
+import example6 from "./example6.png";
 
 const Main = () => {
+  const settings = {
+    autoplay: true,
+    speed: 1000,
+    slidesToShow: 1,
+    arrows: false,
+    slidesToScroll: 1,
+    infinite: true,
+    cssEase: "ease-in-out",
+  };
+
   return (
     <div className="section">
       <MenuList />
-      {/* todo slider */}
-      <img src={mainImage} alt="ourWorks" />
+      <Slider {...settings}>
+        <img src={example} alt="Наши работы" />
+        <img src={example3} alt="Наши работы" />
+        <img src={example5} alt="Наши работы" />
+        <img src={example4} alt="Наши работы" />
+        <img src={example2} alt="Наши работы" />
+        <img src={example6} alt="Наши работы" />
+      </Slider>
     </div>
   );
 };
